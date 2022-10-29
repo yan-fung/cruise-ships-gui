c27fdaf5d17cd3283e45d282ebb0d6dad410edc8
Yan Fung <fungmingyan@hotmail.com>
2022-10-29 23:02:02 +0100

    Added a Controller class and initialiseSea method so that the images of water can be animated.

---
+++ src/controller.js
@@ -0,0 +1,22 @@
+class Controller {
+    constructor() {
+        this.initialiseSea();
+    }
+
+
+    initialiseSea() {
+        const backgrounds = [
+            './images/water0.png',
+            './images/water1.png',
+        ];
+
+        let backgroundIndex = 0;
+        window.setInterval(() => {
+            document.querySelector('#viewport').style.backgroundImage = `url('${backgrounds[backgroundIndex % backgrounds.length]}')`;
+           
+            backgroundIndex += 1;
+        }, 1000);
+
+    };
+
+}
