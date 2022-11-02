d9213ca6cf2d7bc9422ba2afa153d3361d0c3df3
Yan Fung <fungmingyan@hotmail.com>
2022-11-02 11:37:23 +0000

    Wrapped class Ship inside an IIFE

--- src/ship.js
+++ src/ship.js
@@ -1,32 +1,41 @@
-class Ship{
+(function exportShip() {
+    class Ship{
 
-    constructor(itinerary) {
-        this.itinerary = itinerary;
-        this.currentPort = itinerary.ports[0];
-        this.previousPort = null;
-        this.currentPort.addShip(this);
-
-    }
-
-    setSail() {
-        const currentPortIndex = this.itinerary.ports.indexOf(this.currentPort);
-        if (currentPortIndex === this.itinerary.ports.length -1){
-            throw new Error('End of itinerary reached')
+        constructor(itinerary) {
+            this.itinerary = itinerary;
+            this.currentPort = itinerary.ports[0];
+            this.previousPort = null;
+            this.currentPort.addShip(this);
+    
         }
-        // this.currentPort = Dover;
-        this.previousPort = this.currentPort; //Previous port: Dover, Current port: Dover
-        this.currentPort = null;
-        this.previousPort.removeShip(this);
+    
+        setSail() {
+            const currentPortIndex = this.itinerary.ports.indexOf(this.currentPort);
+            if (currentPortIndex === this.itinerary.ports.length -1){
+                throw new Error('End of itinerary reached')
+            }
+            // this.currentPort = Dover;
+            this.previousPort = this.currentPort; //Previous port: Dover, Current port: Dover
+            this.currentPort = null;
+            this.previousPort.removeShip(this);
+        }
+    
+        dock() {
+             const previousPortIndex = this.itinerary.ports.indexOf(this.previousPort);
+             this.currentPort = this.itinerary.ports[previousPortIndex + 1];
+             this.currentPort.addShip(this);
+        }
     }
+    
+    if (typeof module !== 'undefined' && module.exports) {
+        module.exports = Ship;
+      } else {
+        window.Ship = Ship;
+      };  
 
-    dock() {
-         const previousPortIndex = this.itinerary.ports.indexOf(this.previousPort);
-         this.currentPort = this.itinerary.ports[previousPortIndex + 1];
-         this.currentPort.addShip(this);
-    }
-}
+} ())
 
 
 
 
-module.exports = Ship;
+
